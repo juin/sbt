8649c64334eca7aff2f4cdc21d35a2dc5306a3b6
Joabe Pinheiro <joabepinhero@gmail.com>
2015-03-22 18:15:58 -0300

    Merge origin/master
    Conflicts:
    	SBT/src/sbt/SBT.java

--- SBT/src/br/com/iomrh/listeners/CadastrarCandidatoListener.java
+++ SBT/src/br/com/iomrh/listeners/CadastrarCandidatoListener.java
@@ -5,13 +5,23 @@
  */
 package br.com.iomrh.listeners;
 
+import br.com.iomrh.dao.ProfissaoDAO;
+import br.com.iomrh.helpers.MyComboBoxEditor;
+import br.com.iomrh.helpers.MyComboBoxRenderer;
 import java.awt.Component;
+import java.util.List;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
+
 
 /**
  *
@@ -24,8 +34,10 @@ public class CadastrarCandidatoListener {
         ocultarTodasMensagens(jPanels);
     }
     
-
+    /** Oculta todoas as mensagens de validação da tela cadastrar candidato
+     * @param jPanels **/
     public void ocultarTodasMensagens(JPanel[] jPanels){
+        
         
         for (JPanel jPanel : jPanels) {
              Component[] components =   jPanel.getComponents();
@@ -38,6 +50,109 @@ public class CadastrarCandidatoListener {
                 }
             }
         }
-        
     }
+    
+    /** Cria a linha para adição de uma nova formação academic
+     * @param jTable **/
+    public void gerarLinhaFormacaoAcademica(JTable jTable){
+  
+        DefaultTableModel model = (DefaultTableModel) jTable.getModel();
+        
+        //Gerar nivel formação 
+        String[] values = new String[] { "Fundamental", "Médio", "Técnico", "Tecnólogo", "Graduação", "Pós-Graduação", "Mestrado", "Doutorado" };
+
+        TableColumn col = jTable.getColumnModel().getColumn(0);
+        col.setCellEditor(new MyComboBoxEditor(values));
+        col.setCellRenderer(new MyComboBoxRenderer(values));
+        
+        //Gerar status
+        values = new String[] { "Em andamento", "Completo", "Incompleto" };
+
+        col = jTable.getColumnModel().getColumn(3);
+        col.setCellEditor(new MyComboBoxEditor(values));
+        col.setCellRenderer(new MyComboBoxRenderer(values)); 
+        
+        model.addRow(values);
+    }
+    
+    /** Cria a linha para adição de uma nova formação academic
+     * @param jTable **/
+    public void gerarLinhaIdioma(JTable jTable){
+
+        
+        DefaultTableModel model = (DefaultTableModel) jTable.getModel();
+        
+        //Gerar nome da lingua
+        String[] values = new String[] { "Inglês", "Espanhol", "Francês", "Italiano", "Mandarim", "Russo"};
+
+        TableColumn col = jTable.getColumnModel().getColumn(0);
+        col.setCellEditor(new MyComboBoxEditor(values));
+        col.setCellRenderer(new MyComboBoxRenderer(values));
+        
+        //Gerar nivel fala
+        values = new String[] { "Básico", "Intermediário", "Avançado" };
+
+        col = jTable.getColumnModel().getColumn(1);
+        col.setCellEditor(new MyComboBoxEditor(values));
+        col.setCellRenderer(new MyComboBoxRenderer(values));    
+   
+        //Gerar nivel escrita
+        values = new String[] { "Básico", "Intermediário", "Avançado" };
+
+        col = jTable.getColumnModel().getColumn(2);
+        col.setCellEditor(new MyComboBoxEditor(values));
+        col.setCellRenderer(new MyComboBoxRenderer(values));
+        
+        //Gerar nivel leitura
+        values = new String[] { "Básico", "Intermediário", "Avançado" };
+
+        col = jTable.getColumnModel().getColumn(3);
+        col.setCellEditor(new MyComboBoxEditor(values));
+        col.setCellRenderer(new MyComboBoxRenderer(values));
+        
+         model.addRow(new Object[]{});
+    }
+    
+    /** Cria a linha para adição de uma nova formação academic
+     * @param jTable **/
+    public void gerarLinhaInformatica(JTable jTable){
+
+        
+        DefaultTableModel model = (DefaultTableModel) jTable.getModel();
+        
+        //Gerar nome da habilidade
+        String[] values = new String[] { "Excel", "AutoCad", "BrOffice", "Word"};
+
+        TableColumn col = jTable.getColumnModel().getColumn(0);
+        col.setCellEditor(new MyComboBoxEditor(values));
+        col.setCellRenderer(new MyComboBoxRenderer(values));
+        
+        //Gerar nivel 
+        values = new String[] { "Básico", "Intermediário", "Avançado" };
+
+        col = jTable.getColumnModel().getColumn(1);
+        col.setCellEditor(new MyComboBoxEditor(values));
+        col.setCellRenderer(new MyComboBoxRenderer(values));    
+
+         model.addRow(new Object[]{});
+    } 
+    
+    /** Cria a linha para adição de uma nova formação academic
+     * @param jTable **/
+    public void gerarLinhaExperienciaProfissional(JTable jTable){
+
+        ProfissaoDAO profissaoDAO = new ProfissaoDAO();
+        
+        DefaultTableModel model = (DefaultTableModel) jTable.getModel();
+        
+
+        //Gerar nome da habilidade
+        Object[] profissoes = profissaoDAO.listarProfissoes().toArray();
+        
+        TableColumn col = jTable.getColumnModel().getColumn(0);
+        col.setCellEditor(new MyComboBoxEditor(profissoes));
+        col.setCellRenderer(new MyComboBoxRenderer(profissoes));    
+
+        model.addRow(new Object[]{});
+    } 
 }
